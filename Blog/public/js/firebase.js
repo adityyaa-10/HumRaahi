70cfe3a0f8747cf5bbcfb1dd11d3bcf44f1f9f67
Aditya <aditya.pandey.241103@gmail.com>
2022-11-04 20:19:52 +0530

    Blogs

---
+++ Blog/public/js/firebase.js
@@ -0,0 +1,13 @@
+
+let firebaseConfig = {
+  apiKey: "AIzaSyAktlFllfScelaHJxbp07bBOHSviMkxXgo",
+  authDomain: "blogging-website-d52ea.firebaseapp.com",
+  projectId: "blogging-website-d52ea",
+  storageBucket: "blogging-website-d52ea.appspot.com",
+  messagingSenderId: "323791730535",
+  appId: "1:323791730535:web:a08638a2246065198de1c2"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
